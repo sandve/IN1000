ea4d2c757237e8f75e1ae9e026ba4dee1f963893
Geir Kjetil Sandve <geirksa@ifi.uio.no>
2020-08-25 21:27:07 +0200

    Added running example week 1

---
+++ Week1/Running_examples/Capitals/hovedstad_ifelse.py
@@ -0,0 +1,9 @@
+land = input("Velg land i nordre Skandinavia: ")
+if land == "Norge":
+    print("Oslo")
+elif land == "Sverige":
+    print("Stockholm")
+elif land == "Danmark":
+    print("Kobenhavn")
+else:
+    print("Ukjent land")
