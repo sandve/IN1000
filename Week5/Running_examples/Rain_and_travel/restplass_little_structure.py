2a3827fa7520b8de43bf1af8b0ee805a7ffd4ce3
Geir Kjetil Sandve <geirksa@ifi.uio.no>
2020-08-27 14:00:10 +0200

    Added rain and travel running example

---
+++ Week5/Running_examples/Rain_and_travel/restplass_little_structure.py
@@ -0,0 +1,34 @@
+dagsregn = {}
+for linje in open("regn.csv"):
+	biter = linje.split()
+	dato = biter[0]
+	regn = float(biter[1])
+	dagsregn[dato] = regn
+
+dagssok = {}
+for linje in open("trends.csv"):
+	biter = linje.split()
+	dato = biter[0]
+	score = int(biter[1])
+	dagssok[dato] = score
+
+sum_dorlig_ver = 0
+sum_bra_ver = 0
+antall_dager_dorlig = 0
+antall_dager_bra = 0
+
+assert dagsregn.keys() == dagssok.keys()
+datoer = dagsregn.keys()
+
+for dato in datoer:
+	if dagsregn[dato] >2:
+		sum_dorlig_ver += dagssok[dato]
+		antall_dager_dorlig += 1
+	else:
+		sum_bra_ver += dagssok[dato]
+		antall_dager_bra += 1
+
+print("Gjennomsnittlig score dager med bra ver:")
+print( sum_bra_ver / antall_dager_dorlig )
+print("Gjennomsnittlig score dager med daarlig ver:")
+print( sum_dorlig_ver / antall_dager_bra )
